a919f3231a3436a58bc4ff4206e426ee7e501a04
Trevor Ransom <stratogriz@gmail.com>
2012-09-05 14:07:23 -0700

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SpectrumDemo::Application.config.secret_token = 'f83d0fc4e3330de92f10fe95acb8d760c27695b7922fb7d5f79141deb4a1d1805f848d7e4387ba0e76aa4b086e2291f3fdaa077f534c8508f465694d4a1a2f64'
